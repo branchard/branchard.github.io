dc0e67f8a819efc4aef54b0797fc5d50679ac401
branchard <benoit.branchard@gmail.com>
2017-08-31 00:20:53 +0200

    init/config style (SASS)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: `${__dirname}/src/index.js`,
+  entry: {
+	  main: [
+		  `${__dirname}/src/index.js`,
+		  `${__dirname}/src/styles/main.scss`,
+	  ]
+  },
   output: {
     path: `${__dirname}/build`,
     publicPath: '/build/',
@@ -13,9 +19,14 @@ module.exports = {
   module: {
     rules: [
       {test: /.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+	  {test: /\.(css|scss)/, use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])}
     ],
   },
-  plugins: process.argv.indexOf('-p') === -1 ? [] : [
+  plugins: process.argv.indexOf('-p') === -1 ? [
+	  new ExtractTextPlugin({
+            filename: 'style.css',
+        })
+  ] : [
     new webpack.optimize.UglifyJsPlugin({
       output: {
         comments: false,
