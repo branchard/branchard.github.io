adfc9559af5e1d1a509b9f57ee2401e8906ebfd8
branchard <benoit.branchard@gmail.com>
2017-09-01 10:03:13 +0200

    add sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,56 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-// TODO add sourcemap
+let isProduction = process.argv.indexOf('-p') !== -1;
 
 //make plugins list
-let pluginsList = [
-	new ExtractTextPlugin({
-		filename: 'style.css',
-	 })
-]
+let pluginsList = [new ExtractTextPlugin({filename: 'style.css'})]
 
-if(process.argv.indexOf('-p') !== -1){
-	pluginsList.push(
-		new webpack.optimize.UglifyJsPlugin({
-	      output: {
-	        comments: false,
-	      },
-	    })
-	);
+if (isProduction) {
+    pluginsList.push(new webpack.optimize.UglifyJsPlugin({
+        output: {
+            comments: false
+        }
+    }));
 }
 
 module.exports = {
-  entry: {
-	  main: [
-		  `${__dirname}/src/index.js`,
-		  `${__dirname}/src/styles/main.scss`,
-	  ]
-  },
-  output: {
-    path: `${__dirname}/build`,
-    publicPath: '/build/',
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {test: /.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-	  {test: /\.(css|scss)/, use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])}
-    ],
-  },
-  plugins: pluginsList
+    entry: {
+        main: [`${__dirname}/src/index.js`, `${__dirname}/src/styles/main.scss`]
+    },
+    output: {
+        path: `${__dirname}/build`,
+        publicPath: '/build/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.(css|scss)/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: !isProduction
+                            }
+                        }, {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: !isProduction
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: pluginsList
 };
