aa03bbc8c0f4e700fbf7e55b0d8aa47c8ac2170d
branchard <benoit.branchard@gmail.com>
2017-09-06 19:07:13 +0200

    new timeline (not responsive yet)

---
+++ src/ui/components/organisms/Timeline/Timeline.jsx
@@ -0,0 +1,115 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {Container, Row, Col} from 'reactstrap';
+import {Card, CardImg, CardText, CardBlock, CardHeader,
+	CardTitle, CardSubtitle, Button, Badge } from 'reactstrap';
+
+import SkillCard from "../../molecules/SkillCard";
+
+class Timeline extends Component {
+
+	static propTypes = {
+		content: PropTypes.array.isRequired,
+	};
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			timelineHeight: 1000
+		}
+
+		this.cardsClass = "timeline-cards-444";
+
+		this.renderCards = this.renderCards.bind(this);
+		this.componentDidMount = this.componentDidMount.bind(this);
+	}
+
+	componentDidMount(){
+
+		console.log(this.timelineRow);
+		//document.querySelector("#experiences > div > div > div.timeline.row > div:nth-child(1)").clientHeight + 60px
+		console.log(document.querySelectorAll(`.${this.cardsClass}`));
+		let cards = document.querySelectorAll(`.${this.cardsClass}`);
+		let totalHeight = 0;
+
+		let marginHeight = 0;
+		for (let i = 0; i < cards.length / 2; i++) {
+			totalHeight += cards[i].clientHeight + 60;//marginHeight;
+			marginHeight = 60;
+		}
+
+		this.setState({
+			timelineHeight: totalHeight + 40
+		});
+	}
+
+	renderCards(){
+		let cards = [];
+		let that = this;
+		this.props.content.forEach(function(item, i){
+			let detailsList = [];
+			if(item.details){
+				item.details.forEach(function(detail, i){
+					detailsList.push(
+						<li key={i}>{detail}</li>
+					)
+				});
+			}
+
+			let techsBadges = [];
+			if(item.techs){
+				item.techs.forEach(function(tech, i){
+					techsBadges.push(
+						<Badge key={i}>{tech}</Badge>
+					);
+				});
+			}
+
+			cards.push(
+				<Card key={i} tag="li" className={that.cardsClass}>
+					<CardHeader>
+						<div className="date-box">
+							<span className="month">{item.dateBegin[0]}</span>
+							<span className="year">{item.dateBegin[1]}</span>
+						</div>
+						<div className="date-box">
+							<span className="chevron">
+								<i className="fa fa-chevron-right" aria-hidden="true"></i>
+							</span>
+						</div>
+						<div className="date-box">
+							<span className="month">{item.dateEnd[0]}</span>
+							<span className="year">{item.dateEnd[1]}</span>
+						</div>
+						<div className="titles">
+							<CardTitle tag="h3">{item.title}</CardTitle>
+							<CardSubtitle tag="h4">{item.subtitle}</CardSubtitle>
+						</div>
+			        </CardHeader>
+					<CardBlock>
+						<ul>
+							{detailsList}
+						</ul>
+        			</CardBlock>
+					<CardBlock className="techs">
+						{techsBadges}
+					</CardBlock>
+				</Card>
+			);
+		});
+
+		return cards;
+	}
+
+	render(){
+		return(
+			<Row className="timeline" tag="ul" style={{height: `${this.state.timelineHeight}px`}}>
+				{this.renderCards()}
+			</Row>
+		)
+	}
+}
+
+export default Timeline;
--- src/ui/components/pages/Home/Home.jsx
+++ src/ui/components/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 
 import Section from "../../organisms/Section";
 import SkillList from "../../organisms/SkillsList";
-import PdfView from "../../organisms/PdfView";
+import Timeline from "../../organisms/Timeline";
+//import PdfView from "../../organisms/PdfView";
 import Footer from "../../organisms/Footer";
 
 class Home extends Component {
@@ -25,6 +26,86 @@ class Home extends Component {
 					title="Mes expériences"
 					id="experiences"
 				>
+					<Timeline
+						content={[
+							{
+								dateBegin: ["Fév.", "2017"],
+								dateEnd: ["Juil.", "2017"],
+								title: "Développeur web (stage)",
+								subtitle: "ASTMICRO - Gien (45)",
+								details: [
+									"Conception, en autonomie, d'un panel « single page » de gestion de boutique eBay",
+									"Création d’une interface WYSIWYG",
+									"Utilisation de l’API eBay"
+								],
+								techs: [
+									"Node.js",
+									"Meteor",
+									"MongoDB",
+									"React",
+									"Bootstrap 4",
+									"HTML",
+									"CSS",
+									"JavaScript"
+								]
+							},
+							{
+								dateBegin: ["Oct.", "2016"],
+								dateEnd: ["Fév.", "2017"],
+								title: "Développeur web (projet tutoré)",
+								subtitle: "Mme. Brossier - Orléans (45)",
+								details: [
+									"Mise en place d’une interface web pour créer des CV et des lettres de motivation",
+									"Travail en groupe de 7"
+								],
+								techs: [
+									"Silex",
+									"PHP Objet",
+									"SQL",
+									"Twig",
+									"jQuery",
+									"Bootstrap 3",
+									"HTML",
+									"CSS",
+									"JavaScript",
+									"Git"
+								]
+							},
+							{
+								dateBegin: ["Avr.", "2015"],
+								dateEnd: ["Juin", "2015"],
+								title: "Développeur web (stage)",
+								subtitle: "Marie - Dampierre-en-Burly (45)",
+								details: [
+									"Conception d’une interface web de dépôt de fichiers",
+								],
+								techs: [
+									"Django",
+									"Jinja",
+									"Python",
+									"MySQL",
+									"jQuery",
+									"Bootstrap 3",
+									"HTML",
+									"CSS",
+									"JavaScript"
+								]
+							},
+							{
+								dateBegin: ["Nov.", "2014"],
+								dateEnd: ["Mars", "2015"],
+								title: "Développeur mobile (projet tutoré)",
+								subtitle: "Compagnie Aede - Orléans (45)",
+								details: [
+									"Développement d’une application mobile multiplateforme",
+								],
+								techs: [
+									"Apache Cordova",
+									"Javascript"
+								]
+							},
+						]}
+					/>
 				</Section>
 				{/* <PdfView scale={10.0}/> */}
 				<Footer/>
