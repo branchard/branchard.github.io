152f7550a8d862e6673ea01f444b8194b2222fcf
branchard <benoit.branchard@gmail.com>
2017-09-06 20:30:27 +0200

    add trainings section

--- src/ui/components/pages/Home/Home.jsx
+++ src/ui/components/pages/Home/Home.jsx
@@ -110,6 +110,93 @@ class Home extends Component {
 						]}
 					/>
 				</Section>
+				<Section
+					title="Mes expériences"
+					id="trainings"
+					style={{overflow: "auto"}}
+				>
+					<Timeline
+						content={[
+							{
+								dateBegin: ["Fév.", "2017"],
+								dateEnd: ["Juil.", "2017"],
+								title: "Développeur web (stage)",
+								subtitle: "ASTMICRO - Gien (45)",
+								details: [
+									"Conception, en autonomie, d'un panel « single page » de gestion de boutique eBay",
+									"Création d’une interface WYSIWYG",
+									"Utilisation de l’API eBay"
+								],
+								techs: [
+									"Node.js",
+									"Meteor",
+									"MongoDB",
+									"React",
+									"Bootstrap 4",
+									"HTML",
+									"CSS",
+									"JavaScript"
+								]
+							},
+							{
+								dateBegin: ["Oct.", "2016"],
+								dateEnd: ["Fév.", "2017"],
+								title: "Développeur web (projet tutoré)",
+								subtitle: "Mme. Brossier - Orléans (45)",
+								details: [
+									"Mise en place d’une interface web pour créer des CV et des lettres de motivation",
+									"Travail en groupe de 7",
+									"Méthode de développement"
+								],
+								techs: [
+									"Silex",
+									"PHP Objet",
+									"SQL",
+									"Twig",
+									"jQuery",
+									"Bootstrap 3",
+									"HTML",
+									"CSS",
+									"JavaScript",
+									"Git"
+								]
+							},
+							{
+								dateBegin: ["Avr.", "2015"],
+								dateEnd: ["Juin", "2015"],
+								title: "Développeur web (stage)",
+								subtitle: "Marie - Dampierre-en-Burly (45)",
+								details: [
+									"Conception d’une interface web de dépôt de fichiers",
+								],
+								techs: [
+									"Django",
+									"Jinja",
+									"Python",
+									"MySQL",
+									"jQuery",
+									"Bootstrap 3",
+									"HTML",
+									"CSS",
+									"JavaScript"
+								]
+							},
+							{
+								dateBegin: ["Nov.", "2014"],
+								dateEnd: ["Mars", "2015"],
+								title: "Développeur mobile (projet tutoré)",
+								subtitle: "Compagnie Aede - Orléans (45)",
+								details: [
+									"Développement d’une application mobile multiplateforme",
+								],
+								techs: [
+									"Apache Cordova",
+									"Javascript"
+								]
+							},
+						]}
+					/>
+				</Section>
 				{/* <PdfView scale={10.0}/> */}
 				<Footer/>
 			</div>
