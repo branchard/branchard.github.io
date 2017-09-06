727d88e735a3faaea5afc5208b708bd9ccf0a41a
branchard <benoit.branchard@gmail.com>
2017-09-06 14:23:09 +0200

    remove app timeout

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import {render} from 'react-dom';
 //import {BrowserRouter} from 'react-router-dom';
 import App from './ui/App';
 
-setTimeout(function(){
-	render(
-		<App/>,
-		document.getElementById('app'),
-	);
-}, 223200);
+render(
+	<App/>,
+	document.getElementById('app'),
+);
