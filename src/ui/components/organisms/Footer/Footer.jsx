9e2ad15f0d74d0d05afd38433026c607baf4b121
branchard <benoit.branchard@gmail.com>
2017-09-07 14:02:50 +0200

    add cv link in footer

--- src/ui/components/organisms/Footer/Footer.jsx
+++ src/ui/components/organisms/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Button} from 'reactstrap';
 
 const Footer = ({}) => {
 	return(
 		<footer className="page-footer">
 			<Container>
+				<Row>
+					<a href="/assets/CV-benoit-branchard-print.pdf" className="mx-auto btn btn-outline-secondary">Télécharger mon CV</a>
+				</Row>
 				<Row>
 					<span className="col-12 text-center copyright">B. Branchard © 2017.</span>
 				</Row>
